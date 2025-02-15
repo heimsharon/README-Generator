0a383250d4be638c5096c3610409b3dd7520b069
Sharon Heim <heimsharon@hotmail.com>
2025-02-14 20:27:43 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+//Packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import colors from "colors"; //This is optional,allows changes to logged items in CLI.
+import generateMarkdown from "./util/generateMarkdown.js";
+
+//Array of questions for user input.
+const questions = [
+  {
+    type: "input",
+    name: "Title",
+    message: "What would you like to title your project?".magenta,
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "Please describe the project.".magenta,
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message:
+      "Please list/describe installation instructions required for the project.".magenta,
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "When can this project can be used.".magenta,
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message:
+      "Please provide guidelines for others to contribute to the project.".magenta,
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: "Please list/describe test instructions.".magenta,
+  },
+  {
+    type: "list",
+    name: "License",
+    message: "Which license you would like to utilize for this project?".magenta,
+    choices: ["MIT", "Apache", "GPL", "BSD", "None"],
+  },
+  {
+    type: "input",
+    name: "Github",
+    message: "What is your GitHub username?.".magenta,
+  },
+  {
+    type: "input",
+    name: "Email",
+    message:
+      "Please provide your an email address and/or other contact information.".magenta,
+  },
+];
+
+//Function to initialize app.
+
+function init() {
+  inquirer.prompt(questions).then(
+    (answers) => {console.log(answers);
+      fs.writeFile("README.md", generateMarkdown(answers), (err) =>
+        err ? console.error(err.red) : console.log("Success!".green)
+      );
+    },
+    (err) => console.error(err.red)
+  );
+}
+//Initialize the app.
+init();
---
+++ util/generateMarkdown.js
@@ -0,0 +1,78 @@
+//Function that returns a license badge based on users choice.
+// If there is no license,it will  return an empty string.
+function renderLicenseBadge(license) {
+    if (!license || license === "None") {
+      return "";
+    }
+    return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  
+  //Function that returns the license link.
+  //If there is no license, it will return an empty string.
+  function renderLicenseLink(license) {
+    if (!license || license === "None") {
+      return "";
+    }
+    return `[License](https://opensource.org/licenses/${license})`;
+  }
+  
+  //Function that returns the license section of README.
+  //If there is no license,it returns an empty string.
+  function renderLicenseSection(license) {
+    if (!license || license === "None") {
+      return "";
+    }
+    return `## License
+  
+  This project is licensed under the ${license} license.`;
+  }
+  
+  //Function to generate the markdown for the README file.
+  function generateMarkdown(data) {
+    return ` 
+   # ${data.Title}
+  
+  ## Description
+  
+  ${data.Description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.Installation}
+  
+  ## Usage
+  
+  ${data.Usage}
+  
+  ${renderLicenseSection(data.License)}
+  
+  ${renderLicenseBadge(data.License)}
+  
+  ## Contributing
+  
+  ${data.Contributing}
+  
+  ## Tests
+  
+  ${data.Tests}
+  
+  ## Questions
+  
+  If you have any questions, please feel free to contact me at ${data.Email}.
+  
+  You can also find more of my work at [${data.Github}](https://github.com/${
+      data.Github
+    }).
+  `;
+  }
+  
+  export default generateMarkdown;
